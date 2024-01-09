4d785a04c5916bc05736e89a457a988274a3e757
AnjelinArthi12 <156052573+AnjelinArthi12@users.noreply.github.com>
2024-01-09 11:19:49 +0530

    Delete 01_learn.js

--- 01_learn.js
+++
@@ -1,9 +0,0 @@
-let browserName='Chrome'
-function getBrowserVersion(){
-    if(browserName=='Chrome'){
-        let browserVersion = '120.0.6099.131';
-        console.log(browserVersion);
-    }
-}
-
-getBrowserVersion();
